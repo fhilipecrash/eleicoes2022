b27ac4650f93674a5a449ecc9c0307a3b953f19a
Fhilipe Coelho <fhilipecoelho.dev@gmail.com>
2023-02-06 15:06:07 -0300

    `feat`: add get candidadtes by role

--- src/index.mjs
+++ src/index.mjs
@@ -9,16 +9,50 @@ const db = new sqlite3.Database('eleicoes2022-pi.db', (err) => {
 });
 const port = 3000;
 
+function formatRes(rows) {
+  return rows.map(cand => {
+    return {
+      name: cand.cand_nome,
+      role: cand.cargo_nome,
+      votes: cand.cand_votos,
+      status: cand.cand_status === 1 ? 'Eleito' : 'Não Eleito'
+    }
+  });
+}
+
 app.use(express.static('public'));
 
 app.get('/candidate', (req, res) => {
-  db.all("select * from candidato where nome like '%' || ? || '%'", [req.query.q.toUpperCase()], (err, rows) => {
+  const query = `
+    select cand_nome, cargo_nome, cand_votos, cand_status from votos_cand_estado where cand_nome like '%' || ? || '%'
+  `;
+
+  db.all(query, [req.query.name.toUpperCase()], (err, rows) => {
     if (err) {
       throw Error(err.message);
     }
 
-    res.json(rows)
-  })
+    res.json(formatRes(rows));
+  });
+});
+
+app.get('/role/:cargoId', (req, res) => {
+  // 1: Presidente
+  // 3: Governador
+  // 5: Senador
+  // 6: Deputado Federal
+  // 7: Deputado Estadual
+  const query = `
+    select cand_nome, cand_votos, cand_status from votos_cand_estado where cargo_id = ?
+  `;
+
+  db.all(query, [req.params.cargoId], (err, rows) => {
+    if (err) {
+      throw Error(err.message);
+    }
+
+    res.json(formatRes(rows))
+  });
 });
 
 app.listen(port, () => {
